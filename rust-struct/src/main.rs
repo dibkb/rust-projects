09a1426ef2ab35ec06741ba170accf787282e61b
dibkb <dibas9110@gmail.com>
2025-05-20 18:45:51 +0530

    rusty

--- rust-struct/src/main.rs
+++ rust-struct/src/main.rs
@@ -1,3 +1,21 @@
+
+struct Rectangle {
+    height: f64,
+    width: f64
+}
+
+impl Rectangle {
+    fn new(height: f64, width: f64) -> Rectangle {
+        Rectangle { height, width }
+    }
+
+    fn cal_area(&self)-> f64{
+        self.height * self.width
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle::new(5.3, 6.3);
+    println!("rect {:#?}",rect);
+    println!("The area of the rect is {}", rect.cal_area());
 }
