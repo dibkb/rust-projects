6a9f6884c7ba83d6f28e155c92768eb26bfab484
dibkb <dibas9110@gmail.com>
2025-05-21 09:46:15 +0530

    rust

--- rust-struct/src/main.rs
+++ rust-struct/src/main.rs
@@ -1,21 +1,27 @@
-
-struct Rectangle {
-    height: f64,
-    width: f64
-}
-
-impl Rectangle {
-    fn new(height: f64, width: f64) -> Rectangle {
-        Rectangle { height, width }
-    }
-
-    fn cal_area(&self)-> f64{
-        self.height * self.width
-    }
-}
+use std::io::Read;
+use std::fs::File;
+use std::error::Error;
 
 fn main() {
-    let rect = Rectangle::new(5.3, 6.3);
-    println!("rect {:#?}",rect);
-    println!("The area of the rect is {}", rect.cal_area());
+    let user_name = read_username();
+    match user_name {
+        Ok(name)=> println!("{}",name),
+        Err(_) => panic!("Error occured")
+    }
+
+}
+
+fn read_username() -> Result<String, Box<dyn Error>> {
+    let mut file = File::open("./hello.txt");
+    let user_name = match &mut file {
+        Ok(file) => {
+            let mut file_username = String::new();
+            match file.read_to_string(&mut file_username) {
+                Ok(_) => file_username,
+                Err(error) => panic!("Problem reading the contents: {error:?}"),
+            }
+        },
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
+    Ok(user_name)
 }
